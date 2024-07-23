f005023d7fbf256c6375cc00c5c09f3d22d1e6f4
LittleFrog1959 <dgwaine@gmail.com>
2024-07-23 14:15:35 +0100

    First version of express page

--- serverSide.js
+++ serverSide.js
@@ -1,11 +1,24 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require ('express');
+const server = express ();
+const port = process.env.PORT || 1000;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render And Me</title>
+  </head>
+  <body>
+    <section>
+      Hello from dgw
+    </section>
+  </body>
+</html>
+'
