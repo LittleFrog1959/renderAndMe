d41f148659b3ed66e56c7f95ed9d6364c4a3c43b
LittleFrog1959 <dgwaine@gmail.com>
2024-07-23 14:19:13 +0100

    Debugging express version of program

--- serverSide.js
+++ serverSide.js
@@ -1,10 +1,10 @@
 const express = require ('express');
-const server = express ();
+const app = express ();
 const port = process.env.PORT || 1000;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
