559ba576036655d3463a310a5f077b080b218c6b
LittleFrog1959 <dgwaine@gmail.com>
2024-07-24 21:18:03 +0100

    Trying to add a couple of files

--- serverSide.js
+++ serverSide.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 1000;
 
 app.get("/", (req, res) => {
     res.render ('index', {testing: 'hello June'});
+    console.log ("Just printed index.ejs")
 });
 
 const userRouter = require ('./routes/users')
